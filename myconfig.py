3306b12dfb93255a9457333f8fa4d24289bfb04f
ejleighton <80317945+ejleighton@users.noreply.github.com>
2021-04-26 21:30:59 +0100

    Added more sample data
    Added a second sample set for testing. Repo now includes images and shapefiles for Chernobyl Exclusion Zone and Mount St. Helens and example config files to use this data

--- myconfig.py
+++ myconfig.py
@@ -12,9 +12,9 @@ Variables:
     shapefile: path and filename for the shapefile containing the study area boundary
 """
 
-maptitle = 'Difference in NDVI in the Ukraine CEZ from May 1986 to May 1991'
-newRed = 'data_files\\LT05_L1TP_182024_19910513_20180225_01_T1_B3.TIF'  # image 1 visible red
-newNIR = 'data_files\\LT05_L1TP_182024_19910513_20180225_01_T1_B4.TIF'  # image 1 nir
-oldRed = 'data_files\\LT05_L1TP_182024_19860531_20170217_01_T1_B3.TIF'  # image 2 visible red
-oldNIR = 'data_files\\LT05_L1TP_182024_19860531_20170217_01_T1_B4.TIF'  # image 2 nir
-shapefile = 'data_files\\ExclusionZone.shp'  # shapefile of study area
+maptitle = 'Difference in NDVI in the Mt. St. Helens National Volcano Monument from Sept 1986 to July 1995'
+newRed = 'data_files\\MSH\\LT05_L1TP_046028_19950718_20160926_01_T1_B3.TIF'  # image 1 visible red
+newNIR = 'data_files\\MSH\\LT05_L1TP_046028_19950718_20160926_01_T1_B4.TIF'  # image 1 nir
+oldRed = 'data_files\\MSH\\LT05_L1TP_046028_19860911_20161003_01_T1_B3.TIF'  # image 2 visible red
+oldNIR = 'data_files\\MSH\\LT05_L1TP_046028_19860911_20161003_01_T1_B4.TIF'  # image 2 nir
+shapefile = 'data_files\\MSH\\MSH_NVM.shp'  # shapefile of study area
