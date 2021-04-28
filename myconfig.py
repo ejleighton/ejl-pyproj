a535dd816d9b5c54926deafa49d36a2cff71c422
ejleighton <80317945+ejleighton@users.noreply.github.com>
2021-04-28 12:41:40 +0100

    feat: area calculations for change
    Script now classifies ndvi difference into brackets and creates polygons using these brackets. Area of change can then be calculated from these polys.

--- myconfig.py
+++ myconfig.py
@@ -1,20 +1,20 @@
-"""
-This file is for the configuration of paths and filenames to be passed to the main script.
+# This file is for the configuration of paths and filenames to be passed to the main script.
+#
+# All variables must be configured.
+#
+# Variables:
+#    maptitle: text to appear above the final map figure
+#    newRed: path and filename for the red band of the more recent image
+#    newNIR: path and filename for the NIR band of the more recent image
+#    oldRed: path and filename for the red band of the older image
+#    oldNIR: path and filename for the NIR band of the older image
+#    shapefile: path and filename for the shapefile containing the study area boundary
+#	 spatialref: spatial reference for analysis in format 'epsg:#####'
 
-All variables must be configured.
-
-Variables:
-    maptitle: text to appear above the final map figure
-    newRed: path and filename for the red band of the more recent image
-    newNIR: path and filename for the NIR band of the more recent image
-    oldRed: path and filename for the red band of the older image
-    oldNIR: path and filename for the NIR band of the older image
-    shapefile: path and filename for the shapefile containing the study area boundary
-"""
-
-maptitle = 'Difference in NDVI in the Mt. St. Helens National Volcano Monument from Sept 1986 to July 1995'
-newRed = 'data_files\\MSH\\LT05_L1TP_046028_19950718_20160926_01_T1_B3.TIF'  # image 1 visible red
-newNIR = 'data_files\\MSH\\LT05_L1TP_046028_19950718_20160926_01_T1_B4.TIF'  # image 1 nir
-oldRed = 'data_files\\MSH\\LT05_L1TP_046028_19860911_20161003_01_T1_B3.TIF'  # image 2 visible red
-oldNIR = 'data_files\\MSH\\LT05_L1TP_046028_19860911_20161003_01_T1_B4.TIF'  # image 2 nir
-shapefile = 'data_files\\MSH\\MSH_NVM.shp'  # shapefile of study area
+maptitle = 'Difference in NDVI in the Ukraine CEZ from May 1986 to May 1991'
+newRed = 'data_files\\CEZ\\LT05_L1TP_182024_19910513_20180225_01_T1_B3.TIF'  # image 1 visible red
+newNIR = 'data_files\\CEZ\\LT05_L1TP_182024_19910513_20180225_01_T1_B4.TIF'  # image 1 nir
+oldRed = 'data_files\\CEZ\\LT05_L1TP_182024_19860531_20170217_01_T1_B3.TIF'  # image 2 visible red
+oldNIR = 'data_files\\CEZ\\LT05_L1TP_182024_19860531_20170217_01_T1_B4.TIF'  # image 2 nir
+shapefile = 'data_files\\CEZ\\ExclusionZone.shp'  # shapefile of study area
+spatialref = 'epsg:5577'  # espg for Ukraine 2000 3 Degree GK CM 21E
--- ndvidiff.py
+++ ndvidiff.py
@@ -4,6 +4,7 @@ import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 import rasterio as rio
 import rasterio.mask as rmask
+from rasterio.features import shapes
 from mpl_toolkits.axes_grid1 import make_axes_locatable  # for arranging multiple plots, i.e. colorbar axes
 from cartopy.feature import ShapelyFeature
 from rasterio.windows import from_bounds  # thanks https://gis.stackexchange.com/a/336903 for pointing this function out
@@ -126,6 +127,63 @@ def ndvidiff(new, old, outpath):
     return diff
 
 
+# https://gis.stackexchange.com/a/177675
+def classify_change(srcimg, pos_thold, neg_thold, outpath):
+    """
+    Returns classified image using supplied thresholds
+
+        Parameters:
+             srcimg (str): path to raster to be classified
+             pos_thold (float): value above which pixels will be classed as positive change
+             neg_thold (float): value below which pixels will be classed as negative change
+             outpath (str): path and filename of output GeoTiff
+
+        Returns:
+            classified (array): raster reclassified
+    """
+    with rio.open(srcimg) as src:
+        array = src.read()
+        out_meta = src.meta
+    array[np.where(array >= pos_thold)] = 1
+    array[np.where((array <= neg_thold) & (array > -10))] = 2
+    array[np.where((array < pos_thold) & (array > neg_thold))] = 0
+    with rio.open(outpath, "w", **out_meta) as dest:
+        dest.write(array)
+    return array
+
+
+# https://gis.stackexchange.com/a/187883
+def getpolygons(srcimg):
+    """
+
+    :param srcimg:
+    :return:
+    """
+    with rio.Env():
+        with rio.open(srcimg) as src:
+            image = src.read()
+            results = (
+                {'properties': {'raster_val': v}, 'geometry': s}
+                for i, (s, v)
+                in enumerate(
+                    shapes(image, transform=src.transform)))
+    geoms = list(results)
+    gpd_polygonized_raster = gpd.GeoDataFrame.from_features(geoms)
+    gpd_polygonized_raster.set_crs(crs=src.crs, inplace=True)
+    return gpd_polygonized_raster
+
+
+def getarea(poly):
+    """
+
+    :param poly:
+    :return:
+    """
+    geom = poly.to_crs(spatialref)
+    area = (geom.area / 1000000)
+    return area
+
+
 # --------------------------------[ DATASETS ]--------------------------------------
 
 # load the polygon and set CRS to same as the Landsat data
@@ -140,6 +198,9 @@ img1nir = band_clip(newNIR, 'output\\img1nir.tif')
 img2red = band_clip(oldRed, 'output\\img2red.tif')
 img2nir = band_clip(oldNIR, 'output\\img2nir.tif')
 
+outlinearea = getarea(outline)
+print('Study area size: {:.2f} km²'.format(outlinearea[0]))
+print('Study area size (UTM): {:.2f} km²'.format(outline.area[0]/1000000))
 
 # --------------------------------[ BAND MATHS ]--------------------------------------
 
@@ -151,9 +212,20 @@ diffimg = ndvidiff(ndvi1, ndvi2, 'output\\ndvidiff.tif')
 with rio.open('output\\ndvidiff.tif') as img:
     # set any pixel outside the 'outline' polygon to a value of -9999
     masked_diff, mask_transform = rmask.mask(img, outline.geometry, nodata=-9999)
+    mask_meta = img.meta
 # create a masked array with numpy setting -9999 as a 'bad' or out-of-range value
 masked_diff = np.ma.masked_where(masked_diff == -9999, masked_diff)
+with rio.open('output\\masked_diff.tif', "w", **mask_meta) as mskdest:
+    mskdest.write(masked_diff)
 
+classify_change('output\\masked_diff.tif', 0.3, -0.3, 'output\\classified.tif')
+change_polys = getpolygons('output\\classified.tif')
+pos_change_poly = change_polys[change_polys['raster_val'] == 1]
+neg_change_poly = change_polys[change_polys['raster_val'] == 2]
+pos_change_area = getarea(pos_change_poly).sum()
+neg_change_area = getarea(neg_change_poly).sum()
+print('Area of positive change: {:.2f} km²'.format(pos_change_area))
+print('Area of negative change: {:.2f} km²'.format(neg_change_area))
 
 # --------------------------------[ PLOTTING ]--------------------------------------
 
@@ -177,6 +249,12 @@ im = ax.imshow(masked_diff[0], cmap=mycmap, vmin=-0.5, vmax=0.5, transform=myCRS
 # display poly
 outline_disp = ShapelyFeature(outline['geometry'], myCRS, edgecolor='r', facecolor='none', linewidth=3.0)
 ax.add_feature(outline_disp)
+
+pos_change_disp = ShapelyFeature(pos_change_poly['geometry'], myCRS, edgecolor='none', facecolor='b')
+ax.add_feature(pos_change_disp)
+
+neg_change_disp = ShapelyFeature(neg_change_poly['geometry'], myCRS, edgecolor='none', facecolor='r')
+ax.add_feature(neg_change_disp)
 
 # plot gridlines with labels on top and left
 gridlines = ax.gridlines(draw_labels=True)
